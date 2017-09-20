2732175fe55d692c81285e4c8e3858e2934210a6
Dustin Jorge <dustin.jorge@gmail.com>
2017-09-20 12:06:24 -0700

    Fixes to portaudio player implementation:
    move track unload off callback thread
    rewrite track.readAudio to properly notify when done
    change blocking play to correctly wait for stream to stop
    fix calculation for silence padding
    calculate wait time requirements for stream ending
    decrease size of asynchronous playback intermediate buffer
    increase allowed size of forced playback buffer for blocking playback

--- player/playerpa.go
+++ player/playerpa.go
@@ -16,14 +16,19 @@ import (
 	"time"
 )
 
-// multiplier for padding when silence is needed
-const paddingMillis = 2000
+// max expected sample rate, this is used
+// to approximate wait time during blocking
+// playback
+const maxSampleRate = 22050
+
+// number of milliseconds to pad for silence
+const paddingMillis = 1000
 
 // default size for intermediate read buffer
-const rdBuffSize = 20000
+const rdBuffSize = 10000
 
 // maximum buffer size user can set
-const maxBufferSize = 40000
+const maxBufferSize = 60000
 
 // min buffer size user can set
 const minBufferSize = 10
@@ -209,17 +214,15 @@ func (mpgPlayer *mplayer) playMPG(t *track) {
 	// buffer size. This prevents having to hard-code it which
 	// causes issues depending on playback hardware.
 	// if user specifies buffer size in config, however, we
-	// used that.
+	// used that in synchronous playback.
 	done.Add(1)
 	t.Loaded = true
 	if err = stream.Start(); err != nil {
 		coms.Msg(err.Error())
-		t.unload()
+		t.unload(0)
 		return
 	}
 	done.Wait()
-	// delay to allow stream to finish playing
-	<-time.After(150 * time.Millisecond)
 	stream.Stop()
 }
 
@@ -231,8 +234,7 @@ func (t *track) paParams(frames int) (portaudio.StreamParameters, error) {
 	}
 	// create parameters
 	p := portaudio.HighLatencyParameters(nil, out)
-	// single channel output for tts
-	p.Output.Channels = 1
+	p.Output.Channels = t.Channels
 	if frames > 0 {
 		// user defined frames per buffer...
 		p.FramesPerBuffer = frames
@@ -246,49 +248,64 @@ func (t *track) paParams(frames int) (portaudio.StreamParameters, error) {
 
 func (t *track) playCallback(out []int16) {
 	if !t.Loaded {
-		silence := make([]int16, len(out))
-		copy(out, silence)
 		return
 	}
 	// create output bytes
 	audio := make([]byte, len(out)*2)
-	r, _ := t.readAudio(&audio, t.Decoder)
-	if r == 0 {
-		t.unload()
+	r, done := t.readAudio(audio, t.Decoder)
+	//
+	if done {
+		go t.unload(r)
 		return
 	}
 	// read to output
 	if rErr := binary.Read(bytes.NewBuffer(audio), binary.LittleEndian, out); rErr != nil {
 		coms.Msg(rErr.Error())
-		t.unload()
+		go t.unload(0)
 		return
 	}
 	return
 }
 
-func (t *track) unload() {
+// block for remaining seconds based on bytes read
+// from buffer
+func (t *track) wait(r int) {
+	<-time.After(time.Second * time.Duration(r/(2*int(t.Rate))))
+}
+
+func (t *track) unload(r int) {
 	if t.Loaded {
 		t.Loaded = false
+		// wait for stream to complete
+		t.wait(r)
 		done.Done() // unblock
 	}
 }
 
-func (t *track) readAudio(audio *[]byte, rdr io.Reader) (int, error) {
+func (t *track) readAudio(audio []byte, rdr io.Reader) (int, bool) {
 	var r int
+	done := false
 	// read any before data first
 	if t.Before != nil && t.Before.Len() > 0 {
-		return t.Before.Read(*audio)
-	}
-	// attempt to read primary
-	r, _ = rdr.Read(*audio)
-	// if primary is empty, read from After
-	if r == 0 { // primary is empty
+		r, _ = t.Before.Read(audio)
+	} else {
+		// attempt to read primary
+		r, _ = rdr.Read(audio)
+		// check who determines EOF
 		if t.After != nil && t.After.Len() > 0 {
-			return t.After.Read(*audio)
+			// if primary is empty and after is not, after is EOF mark
+			if r == 0 {
+				r, _ = t.After.Read(audio)
+				if r < len(audio) {
+					done = true
+				}
+			}
+		} else if r < len(audio) {
+			done = true
 		}
-		return 0, mpg123.EOF
 	}
-	return r, nil
+	// if after is not defined, we use primary as EOF mark
+	return r, done
 }
 
 func (mpgPlayer *mplayer) syncPlayMPG(t *track, h *mpg123.Decoder) {
@@ -305,17 +322,15 @@ func (mpgPlayer *mplayer) syncPlayMPG(t *track, h *mpg123.Decoder) {
 		coms.Msg(err.Error())
 		return
 	}
-	defer closeStream(stream)
 	stream.Start()
 	defer stream.Stop()
+	defer closeStream(stream)
+	cnt := 0
+	var tm time.Time
 	for {
-		//var r int
 		audio := make([]byte, 2*len(out))
-		var eof error
-		if _, eof = t.readAudio(&audio, h); eof != nil && eof != mpg123.EOF {
-			coms.Msg(eof.Error())
-			return
-		}
+		r, done := t.readAudio(audio, h)
+		cnt += r
 		err := binary.Read(bytes.NewBuffer(audio), binary.LittleEndian, &out)
 		if err != nil {
 			coms.Msg(err.Error())
@@ -325,16 +340,29 @@ func (mpgPlayer *mplayer) syncPlayMPG(t *track, h *mpg123.Decoder) {
 			coms.Msg(err.Error())
 			return
 		}
-		if eof == mpg123.EOF {
-			// give stream a chance to complete
-			<-time.After(time.Millisecond * 300)
-			return
+		// now we've written bytes to the hardware,
+		// start expectation clock
+		if tm.IsZero() {
+			tm = time.Now()
+		}
+		if done {
+			// since we're stuffing the buffer, give stream a chance to complete
+			expected := time.Duration(cnt/(2*int(t.Rate))) * time.Second
+			actual := time.Since(tm)
+			<-time.After(expected - actual)
+			// due to hardware buffering, time may vary...
+			// wait for 1 more full buffer at max samplerate just to be sure
+			// then add "just a little bit" (very accurate)
+			rt := float32(t.Rate)/float32(maxSampleRate) + .125
+			t.wait(int(float32(len(audio)) * rt))
+			break
 		}
 	}
 }
 
 func getSilence(mill int, rate int64) []byte {
-	return make([]byte, int(float64(rate)*float64(mill/1000)))
+	r := float32(int(rate)*2) * float32(mill/1000)
+	return make([]byte, int(r))
 }
 
 func format(dec *mpg123.Decoder, file string) error {
@@ -342,10 +370,10 @@ func format(dec *mpg123.Decoder, file string) error {
 		return err
 	}
 	// format info
-	rate, _, _ := dec.GetFormat()
+	rate, chans, _ := dec.GetFormat()
 	// don't allow format to vary
 	dec.FormatNone()
-	dec.Format(rate, 1, mpg123.ENC_SIGNED_16)
+	dec.Format(rate, chans, mpg123.ENC_SIGNED_16)
 	return nil
 }
 
